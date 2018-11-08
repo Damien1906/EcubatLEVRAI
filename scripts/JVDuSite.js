60193d95e065988b25f0df4c5c317578907b40c3
BaptouDeBellevue <baptiste.daumard@bellevue-lyon>
2018-11-08 11:23:01 +0100

    image chat test

--- scripts/JVDuSite.js
+++ scripts/JVDuSite.js
@@ -13,7 +13,7 @@ function currentSlide(n) {
 
 function showSlides(n) {
   var i;
-  var slides = document.getElementsByClassName("mySlides");
+  var slides = document.getElementsByClassName("chat1");
   var dots = document.getElementsByClassName("dot");
   if (n > slides.length) {slideIndex = 1}
   if (n < 1) {slideIndex = slides.length}

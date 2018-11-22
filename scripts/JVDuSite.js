d844aa11a8b2943779113a654e354e5b7f218ff6
BaptouDeBellevue <baptiste.daumard@bellevue-lyon>
2018-11-22 11:41:31 +0100

    mamy caca

--- scripts/JVDuSite.js
+++ scripts/JVDuSite.js
@@ -25,7 +25,7 @@ function showSlides(n) {
       slides[i].style.display = "none";
   }
   for (i = 0; i < dots.length; i++) {
-      dots[i].className = dots[i].className.replace(" active");
+      dots[i].className = dots[i].className.replace(" active")
   }
   slides[slideIndex-1].style.display = "block";
   dots[slideIndex-1].className += " active";

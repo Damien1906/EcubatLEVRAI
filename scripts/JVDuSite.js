fa60d1913b50d3538bb69877ee4d60da93aa6360
MatMatty <43439252+MatMatty@users.noreply.github.com>
2018-12-13 19:46:10 +0100

    CAROUSSSSSSSSSEL

--- scripts/JVDuSite.js
+++ scripts/JVDuSite.js
@@ -5,29 +5,39 @@ console.log(titre.style)
 var slideIndex = 1;
 showSlides(slideIndex);
 
+// Next/previous controls
 function plusSlides(n) {
- showSlides(slideIndex += n);
+  showSlides(slideIndex += n);
 }
 
+// Thumbnail image controls
 function currentSlide(n) {
- showSlides(slideIndex = n);
+  showSlides(slideIndex = n);
 }
 
 function showSlides(n) {
- var i;
- var slides = document.getElementsByClassName("mySlides");
- var dots = document.getElementsByClassName("dot");
- if (n > slides.length) {slideIndex = 1}
- if (n < 1) {slideIndex = slides.length}
- for (i = 0; i < slides.length; i++) {
-     slides[i].style.display = "none";
- }
- for (i = 0; i < dots.length; i++) {
-     dots[i].className = dots[i].className.replace(" active", "");
- }
- slides[slideIndex-1].style.display = "block";
- dots[slideIndex-1].className += " active";
+  var i;
+  var slides = document.getElementsByClassName("mySlides");
+  var dots = document.getElementsByClassName("dot");
+  if (n > slides.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none";
+  }
+  for (i = 0; i < dots.length; i++) {
+      dots[i].className = dots[i].className.replace(" active")
+  }
+  slides[slideIndex-1].style.display = "block";
+  dots[slideIndex-1].className += " active";
 }
+
+$('.autoplay').slick({
+  slidesToShow: 3,
+  slidesToScroll: 1,
+  autoplay: true,
+  autoplaySpeed: 2000,
+});
+
 
 function openModal() {
   document.getElementsById("modal").style.top = 200px ;
